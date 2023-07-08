055dff2feb067a42c2eb1f7417a410293faa2acb
dongree <dongwoo0307@naver.com>
2023-07-08 21:38:51 +0900

    sanity에 user data 추가

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+import { addUser } from '@/service/user';
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
@@ -11,6 +12,19 @@ const authOptions: NextAuthOptions = {
     // ...add more providers here
   ],
   callbacks: {
+    async signIn({ user: { id, name, email, image } }) {
+      if (!email) {
+        return false;
+      }
+      addUser({
+        id,
+        name: name || '',
+        email,
+        image,
+        username: email.split('@')[0],
+      });
+      return true;
+    },
     async session({ session }) {
       const user = session?.user;
       if (user) {
